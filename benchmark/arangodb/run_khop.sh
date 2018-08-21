4e37bb82b5d77c80078849a944253d750bf86ba2
ainurt <ainur@tigergraph.com>
2018-08-21 00:11:55 +0000

    updated scripts and README

--- benchmark/arangodb/run_khop.sh
+++ benchmark/arangodb/run_khop.sh
@@ -1,32 +1,30 @@
-#!/bin/bash
-
 if [ "$#" -lt 1 ]
 then
   echo "Please provide graph name: graph500 OR twitter"
 elif [ "$1" = "graph500" ]
 then
   echo "Running khop on graph500: k = 1"
-  java -cp .:\* khop graph500 1 180
+  nohup java -cp .:\* khop graph500 1 180
 
   echo "Running khop on graph500: k = 2"
-  java -cp .:\* khop graph500 2 180
+  nohup java -cp .:\* khop graph500 2 180
 
   echo "Running khop on graph500: k = 3"
-  java -cp .:\* khop graph500 3 9000
+  nohup java -cp .:\* khop graph500 3 9000
 
   echo "Running khop on graph500: k = 6"
-  java -cp .:\* khop graph500 6 9000
+  nohup java -cp .:\* khop graph500 6 9000
 elif [ "$1" = "twitter" ]
 then
   echo "Running khop on twitter: k = 1"
-  java -cp .:\* khop twitter 1 180
+  nohup java -cp .:\* khop twitter 1 180
 
   echo "Running khop on twitter: k = 2"
-  java -cp .:\* khop twitter 2 180
+  nohup java -cp .:\* khop twitter 2 180
 
   echo "Running khop on twitter: k = 3"
-  java -cp .:\* khop twitter 3 9000
+  nohup java -cp .:\* khop twitter 3 9000
 
   echo "Running khop on twitter: k = 6"
-  java -cp .:\* khop twitter 6 9000
+  nohup java -cp .:\* khop twitter 6 9000
 fi
