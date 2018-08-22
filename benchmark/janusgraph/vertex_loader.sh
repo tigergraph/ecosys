eb24237115714b0fb8111c6a6dcb62d83618792d
litong <litong@tigergraphs-MacBook-Pro.local>
2018-08-22 11:03:58 -0700

    add storage check in README

--- benchmark/janusgraph/vertex_loader.sh
+++ benchmark/janusgraph/vertex_loader.sh
@@ -1,8 +1,8 @@
 # this script use to load graph500/twitter vertex file and write hashmap (externalId : internalId) to disk
-# CLASSPATH (need to modify): "$CLASSPATH:path/to/lib/*"
-# vertexImporter/class (need to modify): e.g multiThreadVertexImporter
+# CLASSPATH (need to modify): e.g. "$CLASSPATH:/ebs/install/janusgraph/janusgraph-0.2.1-hadoop2/lib/*"
+# vertexImporter/class (need to modify): e.g. multiThreadVertexImporter
 # path/to/Vertex/file (need to modify): e.g. /ebs/raw/twitter_rv/twitter_rv.net_unique_node
-# path/to/Conf/file (need to modify): e.g. /ebs/install/janusgraph/conf/twitter_generate_hashmap.properties
+# path/to/Conf/file (need to modify): e.g. /ebs/install/janusgraph/conf/twitter-janusgraph-cassandra.properties
 
-export CLASSPATH="$CLASSPATH:/ebs/install/janusgraph/janusgraph-0.2.1-hadoop2/lib/*"
+export CLASSPATH="$CLASSPATH:path/to/lib/*"
 java -Xmx96g vertexImporter/class path/to/Vertex/file path/to/Conf/file 4000
