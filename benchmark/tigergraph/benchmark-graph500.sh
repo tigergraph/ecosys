6a9b17ff913c7fcb7150f0a5e6b7dad4a232c274
kcai2TG <kevin.cai@tigergraph.com>
2018-11-26 16:26:25 -0800

    test(benchmark): Automated Testing;

--- benchmark/tigergraph/benchmark-graph500.sh
+++ benchmark/tigergraph/benchmark-graph500.sh
@@ -1,1 +1,6 @@
-nohup python kn.py $1/graph500-22-seed  300 tigergraph 1 notes latency; nohup python kn.py $1/graph500-22-seed  300 tigergraph 2 notes latency; nohup python kn.py $1/graph500-22-seed  10 tigergraph 3 notes latency; nohup python kn.py $1/graph500-22-seed  10 tigergraph 6 notes latency; nohup python wcc.py graph500-22 tigergraph 3;nohup python pg.py graph500-22 tigergraph 10 3 
+nohup python kn.py $1/graph500-22-seed  300 tigergraph 1 notes latency
+nohup python kn.py $1/graph500-22-seed  300 tigergraph 2 notes latency
+nohup python kn.py $1/graph500-22-seed  10 tigergraph 3 notes latency
+nohup python kn.py $1/graph500-22-seed  10 tigergraph 6 notes latency
+nohup python wcc.py graph500-22 tigergraph 3
+nohup python pg.py graph500-22 tigergraph 10 3 
