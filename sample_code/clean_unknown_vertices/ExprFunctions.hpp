7857bfb71725170b6a9b5ae8d75cd56fae1e0d29
Dan Hu <dan.hu@tigergraph.com>
2018-12-21 09:46:22 +0800

    [GF-918] add query to delete unknown vertices issue created by delete by type

---
+++ sample_code/clean_unknown_vertices/ExprFunctions.hpp
@@ -0,0 +1,116 @@
+/******************************************************************************
+ * Copyright (c) 2015-2016, TigerGraph Inc.
+ * All rights reserved.
+ * Project: TigerGraph Query Language
+ * udf.hpp: a library of user defined functions used in queries.
+ *
+ * - This library should only define functions that will be used in
+ *   TigerGraph Query scripts. Other logics, such as structs and helper
+ *   functions that will not be directly called in the GQuery scripts,
+ *   must be put into "ExprUtil.hpp" under the same directory where
+ *   this file is located.
+ *
+ * - Supported type of return value and parameters
+ *     - int
+ *     - float
+ *     - double
+ *     - bool
+ *     - string (don't use std::string)
+ *     - accumulators
+ *
+ * - Function names are case sensitive, unique, and can't be conflict with
+ *   built-in math functions and reserve keywords.
+ *
+ * - Please don't remove necessary codes in this file
+ *
+ * - A backup of this file can be retrieved at
+ *     <tigergraph_root_path>/dev_<backup_time>/gdk/gsql/src/QueryUdf/ExprFunctions.hpp
+ *   after upgrading the system.
+ *
+ ******************************************************************************/
+
+#ifndef EXPRFUNCTIONS_HPP_
+#define EXPRFUNCTIONS_HPP_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+#include <gle/engine/cpplib/headers.hpp>
+
+/**     XXX Warning!! Put self-defined struct in ExprUtil.hpp **
+ *  No user defined struct, helper functions (that will not be directly called
+ *  in the GQuery scripts) etc. are allowed in this file. This file only
+ *  contains user-defined expression function's signature and body.
+ *  Please put user defined structs, helper functions etc. in ExprUtil.hpp
+ */
+#include "ExprUtil.hpp"
+
+namespace UDIMPL {
+  typedef std::string string; //XXX DON'T REMOVE
+
+  /****** BIULT-IN FUNCTIONS **************/
+  /****** XXX DON'T REMOVE ****************/
+  inline int str_to_int (string str) {
+    return atoi(str.c_str());
+  }
+
+  inline int float_to_int (float val) {
+    return (int) val;
+  }
+
+  inline string to_string (double val) {
+    char result[200];
+    sprintf(result, "%g", val);
+    return string(result);
+  }
+
+  inline void RemoveUnknownID(ServiceAPI* api,
+                              EngineServiceRequest& request,
+                              const std::string& vtype,
+                              ListAccum<VERTEX>& vlist,
+                              std::string& msg) {
+    gvector<VertexLocalId_t> ids;
+    gvector<VertexLocalId_t> unknownIds;
+    size_t sz = 0;
+    //scan all found internal ids, batch by batch (10000 per batch)
+    for (auto& vt : vlist.data_) {
+      ++sz;
+      ids.push_back(vt.vid);
+      //sending out one batch
+      if (ids.size() == 10000 || sz == vlist.data_.size()) {
+        std::vector<std::string> uids = std::move(api->VIdtoUId(&request, ids));
+        //Get the offset for current batch
+        size_t offset = sz - ids.size();
+        for (size_t k = 0; k < uids.size(); ++k) {
+          if (uids[k] == "UNKNOWN") {
+            unknownIds.push_back(vlist.data_[offset + k].vid);
+          }
+        }
+        ids.clear();
+      }
+    }
+
+    if (unknownIds.empty()) {
+      std::stringstream ss;
+      ss << "Scanned " << vlist.size() << " \'" << vtype << "\' vertices and no unknown vid has been found, do nothing!!!";
+      msg = ss.str();
+      return;
+    }
+
+    std::stringstream ss;
+    ss << "Scanned " << vlist.size() << " \'" << vtype << "\' vertices, there are "
+       << unknownIds.size() << " unknown vids being removed from engine." << std::endl;
+    gshared_ptr<topology4::GraphUpdates> graphupdates = api->CreateGraphUpdates(&request);
+    size_t vTypeId = api->GetTopologyMeta()->GetVertexTypeId(vtype);
+    graphupdates->BeforeAU();
+    for (auto id : unknownIds) {
+      graphupdates->DeleteVertex(true, topology4::DeltaVertexId(vTypeId, id));
+    }
+    graphupdates->AfterAU();
+    graphupdates->Commit();
+    msg = ss.str();
+  }
+}
+/****************************************/
+
+#endif /* EXPRFUNCTIONS_HPP_ */
