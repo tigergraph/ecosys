c8dc6e0ea1c005553ec9fc9b83c3aabd34b6992b
Jun Hyung Shin <junhyung.shin@tigergraph.com>
2019-02-06 19:38:20 -0800

    Add timing option for batch delta loading time

--- ldbc_benchmark/tigergraph/load_scripts/time.py
+++ ldbc_benchmark/tigergraph/load_scripts/time.py
@@ -2,20 +2,23 @@ import sys
 import os
 import re
 from datetime import datetime, timedelta
+import argparse
 
-def getTime(file):
-  with open(file, 'r') as f:
+DEFAULT_PATH_TO_DELTA_OUT = "/home/tigergraph/tigergraph/logs/delta.out"
+
+def getTimeLoadingJob(file):
+  with open(file, "r") as f:
     end_time_str = ""
     for line in reversed(list(f)):
-      if 'System_GCleanUp|Finished' in line:
+      if "System_GCleanUp|Finished" in line:
         end_time_str = line[:15]
         break
-      elif '*** Aborted at' in line:
+      elif "*** Aborted at" in line:
         end_epoch_str = line[15:25]
         break
     if not end_time_str and not end_epoch_str:
-      return False
-  begin_time_str = re.match(r'.+\.([0-9]+).log', file, re.M).group(1)
+      return -1
+  begin_time_str = re.match(r".+\.([0-9]+).log", file, re.M).group(1)
   begin_time = datetime.fromtimestamp(int(begin_time_str)/1000.0)
   if end_time_str:
     end_time_str = "{}-{:02d}-{:02d} {}".format(begin_time.year, begin_time.month, begin_time.day, end_time_str)
@@ -24,12 +27,45 @@ def getTime(file):
     end_time = datetime.fromtimestamp(int(end_epoch_str))
   if end_time < begin_time:
     end_time = end_time + timedelta(days=1)
-  print("Finished in {} seconds.".format(round((end_time - begin_time).total_seconds(), 3)))
-  return True
+  return round((end_time - begin_time).total_seconds(), 3)
+
+def getTimeBuildGstore(file):
+  with open(file, "r") as f:
+    end_time_str = ""
+    num_line_to_read = 10
+    for line in reversed(list(f)):
+      num_line_to_read -= 1
+      if "All done!" in line:
+        end_time_str = re.match(r".+\(([0-9\.]+) ms\).+", line).group(1)
+        break
+      if num_line_to_read <= 0:
+        break
+    if not end_time_str:
+      return -1
+    else:
+      return round(float(end_time_str)/1000, 3)
 
 if __name__ == "__main__":
-  if len(sys.argv) != 2:
-    print("usage: python3 time.py [FULL_PATH_TO_LOG]")
+  ap = argparse.ArgumentParser()
+  ap.add_argument("loading_job", help="Full path to the loding job log file")
+  ap.add_argument("-d", "--delta", nargs="?", const=DEFAULT_PATH_TO_DELTA_OUT, help="Full path to the gstore build log file, delta.out")
+
+  args = ap.parse_args()
+  loading_job_time = getTimeLoadingJob(args.loading_job)
+  if loading_job_time > 0:
+    print("- Loading job: {} s".format(loading_job_time))
   else:
-    if not getTime(sys.argv[1]):
-      print(sys.argv[1] + " is still in progress. Please come back later.")
+    print("Loading job is still in progress. Please come back later.")
+
+  try:
+    if args.delta != None:
+      building_gstore_time = getTimeBuildGstore(args.delta)
+      if building_gstore_time > 0:
+        print("- Building gstore: {} s".format(building_gstore_time))
+        print("- Total: {} s".format(loading_job_time + building_gstore_time))
+      else:
+        print("Build gstore is still in progress. Please come back later.")
+  except FileNotFoundError:
+    print("No such file: " + args.delta)
+  except:
+    print("Something went wrong.")
