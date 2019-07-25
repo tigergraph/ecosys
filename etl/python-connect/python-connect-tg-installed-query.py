d5cf622147c157bde4e793a2bae5f8df3ad7fc2a
Mingxi Wu <mingxiw@users.noreply.github.com>
2019-07-25 10:56:33 -0700

    Update python-connect-tg-installed-query.py

--- etl/python-connect/python-connect-tg-installed-query.py
+++ etl/python-connect/python-connect-tg-installed-query.py
@@ -2,6 +2,7 @@ import requests
 import json
 
 #url of the REST end point of the installed query hello
+# It is in this general format http://server_ip:9000/query/graphname/queryname
 url ="http://localhost:9000/query/social/hello"
 #query parameters
 params = {"p": "Tom"}
