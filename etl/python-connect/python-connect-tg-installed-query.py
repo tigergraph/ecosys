21ccef68f50aec53317b23803bd1ad22d62b4625
Mingxi Wu <mingxi.wu@tigergraph.com>
2019-07-24 21:23:12 -0700

    add python compiled call

---
+++ etl/python-connect/python-connect-tg-installed-query.py
@@ -0,0 +1,13 @@
+import requests
+import json
+
+#url of the REST end point of the installed query hello
+url ="http://localhost:9000/query/social/hello"
+#query parameters
+params = {"p": "Tom"}
+#invoke the REST call
+response = requests.request("GET", url,  params=params, auth=('tigergraph', 'tigergraph'))
+#put the response in a JSON object
+todos = json.loads(response.text)
+#output the JSON
+print (json.dumps(todos, indent=2))
