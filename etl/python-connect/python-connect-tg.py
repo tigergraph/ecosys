5925255ddac03fcd4e145d05eabee2bd2240dda4
Mingxi Wu <mingxiw@users.noreply.github.com>
2019-07-24 17:06:17 -0700

    Update python-connect-tg.py

--- etl/python-connect/python-connect-tg.py
+++ etl/python-connect/python-connect-tg.py
@@ -4,9 +4,15 @@
 import requests
 import json
 
+#your TigerGraph host machine URL
 url = "http://localhost:14240/gsqlserver/interpreted_query"
+#your GSQLK query
 payload = "INTERPRET QUERY (int a, int b) FOR GRAPH social { start = {person.*}; PRINT start; print a, b; }"
+#query parameters
 params = {"a": 10, "b": 4}
+#http request  with user/password
 response = requests.request("POST", url, data=payload, params=params, auth=('tigergraph', 'tigergraph'))
+#convert query result into JSON object
 todos = json.loads(response.text)
+#print JSON in indent 2 format
 print (json.dumps(todos, indent=2))
