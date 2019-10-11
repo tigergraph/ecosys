9aa3b2ee0a66938430a0b20b1e9b26d5b26ff9a5
ChangranLiu <51983783+ChangranLiu@users.noreply.github.com>
2019-10-11 15:47:34 -0700

    Update iteration.sh

--- guru_scripts/guru19_deep_learning/iteration.sh
+++ guru_scripts/guru19_deep_learning/iteration.sh
@@ -2,7 +2,7 @@
 cnt=0
 while [ $cnt -lt 200 ]; do
     cnt=$((cnt+1))
-    echo "iter: $cnt" &>>Output_validation.txt
+    echo "iter: $cnt"
     gsql -g NeuralNetwork "run query training(1)"
     gsql -g NeuralNetwork "run query prediction_accuracy()"
 done
