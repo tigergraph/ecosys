14670ba81344de481341a8fc1d5f1eda0cf15b50
DanHu <dan.hu@tigergraph.com>
2020-01-31 16:16:25 -0800

    add new query to find unknown vertices and printout

--- sample_code/data_explore/ExprFunctions.hpp
+++ sample_code/data_explore/ExprFunctions.hpp
@@ -72,6 +72,95 @@ namespace UDIMPL {
     return v.vid >> (api->GetTopologyMeta()->segmentsizeinpowerof2_);
   }
 
+//ExprFunction.hpp
+inline int64_t PrintUnknownVertices(ServiceAPI* api,
+                            EngineServiceRequest& request,
+                            ListAccum<VERTEX>& vlist,
+                            const std::string& vtype,
+                            int64_t pos,
+                            const std::string& prefix,
+                            const std::string& outputfolder) {
+  std::fstream fs;
+  fs.open(outputfolder.c_str(), std::fstream::out);
+  if (!fs.is_open()) {
+    GEngineInfo(InfoLvl::Brief, "PrintUnknownVertices") 
+      << "Open filename: " 
+      << outputfolder 
+      << " failed, quit!!!";
+    return 0;
+  }
+  fs.close();
+  //scan all found internal ids, batch by batch (10000 per batch)
+  GEngineInfo(InfoLvl::Brief, "PrintUnknownVertices") 
+      << "The total \'" 
+      << vtype 
+      << "\' vertices that need to be scanned is: "
+      << vlist.size();
+  gvector<VertexLocalId_t> ids;
+  gvector<VertexLocalId_t> unknownIds;
+  size_t sz = 0;
+  uint32_t batch = 0;
+  for (auto& vt : vlist.data_) {
+    ++sz;
+    ids.push_back(vt.vid);
+    //sending out one batch
+    if (ids.size() == 10000 || sz == vlist.data_.size()) {
+      ++batch;
+      std::vector<std::string> uids = std::move(api->VIdtoUId(&request, ids));
+      //Get the offset for current batch
+      size_t offset = sz - ids.size();
+      for (size_t k = 0; k < uids.size(); ++k) {
+        if (uids[k] == "UNKNOWN") {
+          unknownIds.push_back(vlist.data_[offset + k].vid);
+        }
+      }
+      GEngineInfo(InfoLvl::Brief, "PrintUnknownVertices") 
+          << "Finished scan " 
+          << batch 
+          << "-th batch with vertex size = " 
+          << ids.size() 
+          << " and the total unknown vertices size = " 
+          << unknownIds.size();
+      ids.clear();
+    }
+  }
+
+  if (unknownIds.empty()) {
+    GEngineInfo(InfoLvl::Brief, "PrintUnknownVertices") 
+        << "No unknown vertex has been found, do nothing!!!";
+    return 0;
+  }
+
+  GEngineInfo(InfoLvl::Brief, "PrintUnknownVertices") 
+      << "There are total " 
+      << unknownIds.size() 
+      << " vertices have been found, start write data into target folder: " 
+      << outputfolder;
+
+  //open file for output
+  fs.open(outputfolder.c_str(), std::fstream::out);
+  if (fs.is_open()) {
+    auto graphAPIptr = api->CreateGraphAPI(&request);
+    VertexAttribute attr;
+    for (auto id : unknownIds) {
+      if (graphAPIptr->GetOneVertex(id, attr)) {
+        fs << id << ',' << attr.GetString(pos) << '\n';
+      } else {
+        GEngineInfo(InfoLvl::Brief, "PrintUnknownVertices") 
+            << "Invalid vertex with iid: " << id;
+      }
+    }
+    fs.close();
+    GEngineInfo(InfoLvl::Brief, "PrintUnknownVertices") 
+      << "all done!!!";
+  } else {
+    GEngineInfo(InfoLvl::Brief, "PrintUnknownVertices") 
+      << "Open filename: " 
+      << outputfolder 
+      << " failed, quit!!!";
+  }
+  return unknownIds.size();
+}
 }
 /****************************************/
 
