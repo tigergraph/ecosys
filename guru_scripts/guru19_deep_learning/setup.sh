8ac23fd6c6c930130e3fa054971094f3ac45093d
Jun Hyung Shin <junhyung.shin@tigergraph.com>
2020-02-05 14:53:17 -0800

    Revert "Merge branch 'master' into ldbc"
    This reverts commit 79ce5a51954dd2fe7cbe6a1c9439489a17faa119, reversing
    changes made to 55495cff51824c61d9f5df21867414bf1865bc51.

--- guru_scripts/guru19_deep_learning/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-gsql -g NeuralNetwork "DROP QUERY ALL"
-gsql -g NeuralNetwork ./query/training.gsql
-gsql -g NeuralNetwork ./query/prediction_accuracy.gsql
-gsql -g NeuralNetwork ./query/prediction_sketchpad.gsql
-gsql -g NeuralNetwork install query all
-
-
