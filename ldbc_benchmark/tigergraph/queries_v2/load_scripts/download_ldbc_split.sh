226cb8a5ee70115215adf14c4853c39d760b23b7
yczhang1017 <zhangyuchen12344@gmail.com>
2020-08-26 17:40:49 -0700

    update download script

--- ldbc_benchmark/tigergraph/queries_v2/load_scripts/download_ldbc_split.sh
+++ ldbc_benchmark/tigergraph/queries_v2/load_scripts/download_ldbc_split.sh
@@ -4,9 +4,18 @@
 # All rights reserved
 # Author: yuchen.zhang@tigergraph.com
 ################################################
+# ./download_ldbc_split.sh [split index] [number of split] [directory] 
+# [split index] is from 0 to [number of split] - 1 
+# Examples: 
+# Download the first split of 1/24 of data to ldbc_snb_24 for use in 24 machines
+# ./download_ldbc_split.sh 0 24 ldbc_snb_24 
+# 
+# to download the first split for 24 machines in background
+# nohup ./download_ldbc_split.sh 0 24 ldbc_snb_24 > foo.out 2>&1 < /dev/null &
+# to download the first split for 16 machines in background
+# nohup ./download_ldbc_split.sh 0 16 ldbc_snb_16 > foo.out 2>&1 < /dev/null &
 
-# machine number i, i is from [0, 1, ...23]
-# nohup ./download_ldbc.sh i > foo.out 2>&1 < /dev/null &
+################################################
 
 #8 types, 4 types/line
 static_types="organisation place tag tagclass 
@@ -20,11 +29,19 @@ person_knows_person person_likes_comment person_likes_post comment_replyOf_comme
 comment_replyOf_post person_studyAt_organisation person_workAt_organisation"
 
 root=s3://ldbc-snb-datagen-store/results/params-csv-basic-sf10000/runs/20200606_131341/social_network
-n=$1
-s=$((n*101))
-e=$((s+100))
-des=ldbc_snb_6t
-echo "copying to $des for node $n"
+n=${1:-0}
+split=${2:-24}
+des=${3:-ldbc_snb_6t}
+
+all=2424
+s=$((all/split*n))
+e=$((s + all/split - 1))
+if [ $n -eq $((split-1)) ]; then
+  e=$((all-1))
+fi
+
+echo "Copying to $des for node $n"
+echo "Download csv files from $s to $e"
 for t in $static_types; do
   for i in $(seq $s $e); do
     f=$des/$t/${t}_${i}_0.csv
