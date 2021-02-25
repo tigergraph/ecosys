d269fe3e3adf9058e6426cce947211b6aeabf21c
Sung Rim Huh <sungrim.huh@tigergraph.com>
2021-02-25 12:44:11 -0800

    add running workload in the new driver

--- ldbc_benchmark/tigergraph/v0.4.0/driver.py
+++ ldbc_benchmark/tigergraph/v0.4.0/driver.py
@@ -6,16 +6,97 @@ import os
 import subprocess
 import sys
 from pathlib import Path
+from timeit import default_timer as timer
 
 import requests
 
 
-# TODO: Merge load-query.py file.
-# TODO: Parse the result.
-# TODO: Keep running other queries when one query fails.
-# TODO: Get query information from TigerGraph directly with /gsqlserver/gsql/queryinfo endpoint.
-# TODO: Run query in detached mode.
-# TODO: Time the query.
+class ResultWithElapsed:
+    def __init__(self, result, elapsed):
+        self.result = result
+        self.elapsed = elapsed
+
+
+class Query:
+
+    ENDPOINT = 'http://127.0.0.1:9000/query/ldbc_snb/'
+
+    def __init__(self, name, transform_parameter=None):
+        self.name = name
+        self.transform_parameter = transform_parameter
+
+    def run(self, parameter):
+        if self.transform_parameter is not None:
+            parameter = self.transform_parameter(parameter)
+
+        start = timer()
+        response = requests.get(self.ENDPOINT + self.name, params=parameter).json()
+        end = timer()
+
+        if response['error']:
+            raise Error(response['message'])
+
+        return ResultWithElapsed(response['results'], end - start)
+
+
+class Workload:
+    def __init__(self, name, queries, transform_result):
+        self.name = name
+        self.queries = queries
+        self.transform_result = transform_result
+
+    def run(self, parameter):
+        results = [query.run(parameter) for query in self.queries]
+        result = self.transform_result([r.result for r in results])
+        elapsed = sum(r.elapsed for r in results)
+        return ResultWithElapsed(result, elapsed)
+
+
+def transform_parameter_bi1(parameter):
+    return {'date': parameter['datetime']}
+
+
+class ResultTransform:
+    def __init__(self):
+        self.transforms = []
+
+    def __call__(self, results):
+        for transform in self.transforms:
+            results = transform(results)
+        return results
+
+    def __getitem__(self, key):
+        def transform_result(results):
+            return results[key]
+        self.transforms.append(transform_result)
+        return self
+
+
+    def change_key(self, key_map):
+        def transform_result(results):
+            return {key_map.get(k, k): v for k, v in results.items()}
+        self.transforms.append(transform_result)
+        return self
+
+
+    def del_keys(self, keys):
+        def transform_result(results):
+            return [
+                {k: v for k, v in result.items() if k not in keys}
+                for result in results
+            ]
+        self.transforms.append(transform_result)
+        return self
+
+    def change_keys(self, key_map):
+        def transform_result(results):
+            return [
+                {key_map.get(k, k): v for k, v in result.items()}
+                for result in results
+            ]
+        self.transforms.append(transform_result)
+        return self
+
 
 DATA_NAMES = [
     'Comment',
@@ -38,136 +119,110 @@ DATA_NAMES = [
     'Tag',
 ]
 
-ENDPOINT = 'http://127.0.0.1:9000/query/ldbc_snb/'
-
 SCRIPT_DIR_PATH = Path(__file__).resolve().parent
 
-WORKLOADS = {
-    'bi1': {
-        'queries': {
-            'bi1': {},
-        },
-    },
-    'bi2': {
-        'queries': {
-            'bi2': {},
-        },
-    },
-    'bi3': {
-        'queries': {
-            'bi3': {},
-        },
-    },
-    'bi4': {
-        'queries': {
-            'bi4': {},
-        },
-    },
-    'bi5': {
-        'queries': {
-            'bi5': {},
-        },
-    },
-    'bi6': {
-        'queries': {
-            'bi6': {},
-        },
-    },
-    'bi7': {
-        'queries': {
-            'bi7': {},
-        },
-    },
-    'bi8': {
-        'queries': {
-            'bi8': {},
-        },
-    },
-    'bi9': {
-        'queries': {
-            'bi9': {},
-        },
-    },
-    'bi10': {
-        'queries': {
-            'bi10': {},
-        },
-    },
-    'bi11': {
-        'queries': {
-            'bi11': {},
-        },
-    },
-    'bi12': {
-        'queries': {
-            'bi12': {},
-        },
-    },
-    'bi13': {
-        'queries': {
-            'bi13': {},
-        },
-    },
-    'bi14': {
-        'queries': {
-            'bi14': {},
-        },
-    },
-    'bi15': {
-        'queries': {
-            'bi15': {},
-            'bi15_add_weighted_edges': {},
-            'bi15_delete_weighted_edges': {},
-        },
-    },
-    'bi16': {
-        'queries': {
-            'bi16': {},
-        },
-    },
-    'bi17': {
-        'queries': {
-            'bi17': {},
-        },
-    },
-    'bi18': {
-        'queries': {
-            'bi18': {},
-        },
-    },
-    'bi19': {
-        'queries': {
-            'bi19': {},
-            'bi19_add_weighted_edges': {},
-            'bi19_delete_weighted_edges': {},
-        },
-    },
-    'bi20': {
-        'queries': {
-            'bi20': {},
-        },
-    },
-}
+WORKLOADS = [
+    Workload('bi1', [Query('bi1', transform_parameter_bi1)], ResultTransform()[0][0]['@@result']),
+    Workload('bi2', [Query('bi2')], ResultTransform()[0][0]['@@result']),
+    Workload('bi3', [Query('bi3')], ResultTransform()[0][0]['@@result'].change_keys({
+        'forumId': 'forum.id',
+        'forumTitle': 'forum.title',
+        'forumCreationDate': 'forum.creationDate',
+        'personId': 'person.id',
+    })),
+    Workload('bi4', [Query('bi4')], ResultTransform()[0][0]['@@result'].change_keys({
+        'personId': 'person.id',
+        'personFirstName': 'person.firstName',
+        'personLastName': 'person.lastName',
+        'personCreationDate': 'person.creationDate',
+    })),
+    Workload('bi5', [Query('bi5')], ResultTransform()[0][0]['@@result'].change_keys({
+        'personId': 'person.id',
+    })),
+    Workload('bi6', [Query('bi6')], ResultTransform()[0][0]['@@result'].change_keys({
+        'personId': 'person.id',
+    })),
+    Workload('bi7', [Query('bi7')], ResultTransform()[0][0]['@@result'].change_keys({
+        'relatedTagName': 'relatedTag.name',
+        'replyCount': 'count',
+    })),
+    Workload('bi8', [Query('bi8')], ResultTransform()[0][0]['@@result'].del_keys(['totalScore']).change_keys({
+        'personId': 'person.id',
+    })),
+    Workload('bi9', [Query('bi9')], ResultTransform()[0][0]['@@result'].change_keys({
+        'personId': 'person.id',
+        'personFirstName': 'person.firstName',
+        'personLastName': 'person.lastName',
+    })),
+    Workload('bi10', [Query('bi10')], ResultTransform()[0][0]['result'].change_keys({
+        'expertCandidatePersonId': 'expertCandidatePerson.id',
+        'tagName': 'tag.name',
+    })),
+    Workload('bi11', [Query('bi11')], ResultTransform()[0][0].change_key({'@@count': 'count'})),
+    Workload('bi12', [Query('bi12')], ResultTransform()[0][0]['@@result']),
+    Workload('bi13', [Query('bi13')], ResultTransform()[0][0]['@@result'].change_keys({
+        'zombieId': 'zombie.id',
+    })),
+    Workload('bi14', [Query('bi14')], ResultTransform()[0][0]['@@result'].change_keys({
+        'person1Id': 'person1.id',
+        'person2Id': 'person2.id',
+        'city1Name': 'city1.name',
+    })),
+    Workload(
+        'bi15',
+        [
+            Query('bi15_add_weighted_edges'),
+            Query('bi15'),
+            Query('bi15_delete_weighted_edges'),
+        ],
+        ResultTransform()[1][0]['@@result'].change_keys({'personId': 'person.id'}),
+    ),
+    Workload('bi16', [Query('bi16')], ResultTransform()[0][0]['@@result'].del_keys(['totalMessageCount']).change_keys({
+        'personId': 'person.id',
+    })),
+    Workload('bi17', [Query('bi17')], ResultTransform()[0][0]['@@result'].change_keys({
+        'person1Id': 'person1.id',
+    })),
+    Workload('bi18', [Query('bi18')], ResultTransform()[0][0]['@@result'].change_keys({
+        'person2Id': 'person2.id',
+    })),
+    Workload(
+        'bi19',
+        [
+            Query('bi19_add_weighted_edges'),
+            Query('bi19'),
+            Query('bi19_delete_weighted_edges'),
+        ],
+        ResultTransform()[1][0]['@@result'].change_keys({
+            'person1Id': 'person1.id',
+            'person2Id': 'person2.id',
+        }),
+    ),
+    Workload('bi20', [Query('bi20')], ResultTransform()[0][0]['@@result'].change_keys({
+        'person1Id': 'person1.id',
+    })),
+]
 
 
-def get_parameters_file_path(parameters_path, query_name):
-    '''Returns the parameters file name of the given query name.'''
-    return parameters_path / f'{query_name[:2]}-{query_name[2:]}.txt'
+def load_parameters(parameters_dir, workload):
+    '''Loads parameters for the given workload in the given directory.'''
+    parameters = []
 
-
-def parse_parameters_file(parameters_file_name):
-    '''Parses the given file name as a list of parameters.'''
-
-    with open(parameters_file_name, 'r') as f:
-        parameters = []
+    parameters_file_path = parameters_dir / f'{workload.name[:2]}-{workload.name[2:]}.txt'
+    with open(parameters_file_path, 'r') as parameters_file:
         names = []
         types = []
 
-        for line in f:
+        for line in parameters_file:
+
+            line = line.rstrip('\n')
+
             if len(names) == 0:
                 # First line; parse the name and type.
                 names, types = zip(*(w.split(':') for w in line.split('|')))
                 continue
+
+            parameter = {}
 
             for name, param_type, value in zip(names, types, line.split('|')):
                 if param_type.endswith('[]'):
@@ -179,20 +234,12 @@ def parse_parameters_file(parameters_file_name):
 
             parameters.append(parameter)
 
-        return parameters
-
-
-def print_usage_from(parser):
-    '''Returns a function that prints the usage from given parser when called.'''
-    def fn(args):
-        parser.print_usage()
-    return fn
+    return parameters
 
 
 def load_schema(args):
     '''Loads the schema.'''
     subprocess.run(['gsql', 'schema.gsql'])
-    # Check for errors?
 
 
 def load_query(args):
@@ -200,13 +247,13 @@ def load_query(args):
     gsql = ''
 
     for workload in args.workload:
-        workload_path = (SCRIPT_DIR_PATH / 'workloads' / f'{workload}.gsql').resolve()
+        workload_path = (SCRIPT_DIR_PATH / 'workloads' / f'{workload.name}.gsql').resolve()
         gsql += f'@{workload_path}\n'
 
     queries_to_install = [
-        query
+        query.name
         for workload in args.workload
-        for query in WORKLOADS[workload]['queries'].keys()
+        for query in workload.queries
     ]
     gsql += f'INSTALL QUERY {", ".join(queries_to_install)}\n'
 
@@ -228,22 +275,32 @@ def load_all(args):
     load_data(args)
 
 
+def run(args):
+    '''Runs the given workload(s).'''
+    for workload in args.workload:
+        parameters = load_parameters(args.parameters_dir, workload)
+        for parameter in parameters:
+            result = workload.run(parameter)
+            print(result.result)
+            print(result.elapsed)
+
+
 def get_parser():
     # The top-level parser.
     main_parser = argparse.ArgumentParser(description='Various utilities working with GSQL for LDBC SNB.')
-    main_parser.set_defaults(func=print_usage_from(main_parser))
+    main_parser.set_defaults(func=lambda _: main_parser.print_usage())
     main_subparsers = main_parser.add_subparsers(dest='cmd')
 
     # The parser for loading.
     load_parser = main_subparsers.add_parser('load', help='Load the schema, queries, or data.')
-    load_parser.set_defaults(func=print_usage_from(load_parser))
+    load_parser.set_defaults(func=lambda _: load_parser.print_usage())
     load_subparsers = load_parser.add_subparsers(dest='cmd_load')
 
     load_schema_parser = load_subparsers.add_parser('schema', help='Load the LDBC SNB schema.')
     load_schema_parser.set_defaults(func=load_schema)
 
     load_query_parser = load_subparsers.add_parser('query', help='Load queries for workload(s).')
-    load_query_parser.add_argument('workload', nargs='*', help='Workloads to load queries from.')
+    load_query_parser.add_argument('workload', nargs='*', help='The workload to load queries from.')
     load_query_parser.set_defaults(func=load_query)
 
     load_data_parser = load_subparsers.add_parser('data', help='Load data.')
@@ -254,22 +311,30 @@ def get_parser():
     load_all_parser.add_argument('data_dir', type=Path, help='The directory to load data from.')
     load_all_parser.set_defaults(func=load_all)
 
+    # The parser for running.
+    run_parser = main_subparsers.add_parser('run', help='Run the workload(s).')
+    run_parser.add_argument('parameters_dir', type=Path, help='The directory to load parameters from.')
+    run_parser.add_argument('workload', nargs='*', help='The workload to run.')
+    run_parser.set_defaults(func=run)
+
     return main_parser
 
 
 def check_args(args):
-    if args.cmd_load == 'query' or args.cmd_load == 'all':
-        if len(args.workload) == 0:
-            args.workload = set(WORKLOADS.keys())
+    if args.cmd == 'load' and (args.cmd_load == 'query' or args.cmd_load == 'all') or args.cmd == 'run':
+        # If the command is all, the workload may not be defined.
+        if len(getattr(args, 'workload', [])) == 0:
+            # The default is all workloads.
+            args.workload = WORKLOADS
         else:
             actual = set(args.workload)
-            expected = set(WORKLOADS.keys())
+            expected = set(w.name for w in WORKLOADS)
             remaining = actual - expected
             if len(remaining) > 0:
-                raise ValueError(f'Invalid workload {", ".join(remaining)}.')
-            args.workload = actual
+                raise ValueError(f'Invalid workload {", ".join(sorted(remaining))}.')
+            args.workload = [w for w in WORKLOADS if w.name in actual]
 
-    if args.cmd_load == 'data' or args.cmd_load == 'all':
+    if args.cmd == 'load' and (args.cmd_load == 'data' or args.cmd_load == 'all'):
         missing = []
         for name in DATA_NAMES:
             file_path = (args.data_dir / name).with_suffix('.csv')
