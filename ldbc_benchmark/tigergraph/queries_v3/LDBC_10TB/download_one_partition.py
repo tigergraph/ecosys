94e0fda3f804ffc1321f24f6f1f869313f3b7d04
Yuchen Zhang <yuchen.zhang@tigergraph.com>
2021-07-29 09:29:45 -0700

    fix

--- ldbc_benchmark/tigergraph/queries_v3/LDBC_10TB/download_one_partition.py
+++ ldbc_benchmark/tigergraph/queries_v3/LDBC_10TB/download_one_partition.py
@@ -10,109 +10,105 @@ parser.add_argument('index', type=int, help='index of the node')
 parser.add_argument('nodes', type=int, help='the total number of nodes')
 args = parser.parse_args()
 
-pool = Pool(processes=cpu_count())
-client = storage.Client()  
-gcs_bucket = client.bucket(bucket)
-def download(job):
-  blob_name, target = job
-  gcs_bucket.blob(blob_name).download_to_filename(target)
-  
-def download_one_partition(data):
-  buckets = {
+buckets = {
     '10t': 'ldbc_snb_10k',
     '30t': 'ldbc_snb_30k',}
-  roots = {
-    '10t':'v1/results/sf10000-compressed/runs/20210713_203448/social_network/csv/bi/composite-projected-fk/',
-    '30t':'results/sf30000-compressed/runs/20210728_061923/social_network/csv/bi/composite-projected-fk/'}
-  targets = {
-    '10t':'sf10k',
-    '30t':'sf30k'}
-  bucket = buckets[data]
-  root = roots[data]
-  target = Path(targets[data])
+roots = {
+  '10t':'v1/results/sf10000-compressed/runs/20210713_203448/social_network/csv/bi/composite-projected-fk/',
+  '30t':'results/sf30000-compressed/runs/20210728_061923/social_network/csv/bi/composite-projected-fk/'}
+targets = {
+  '10t':'sf10k',
+  '30t':'sf30k'}
+bucket = buckets[args.data]
+root = roots[args.data]
+target = Path(targets[args.data])
 
-  PARTITION_OR_NOT = {
-    'initial_snapshot': True,
-    'inserts_split': True,
-    'deletes': False,}
+PARTITION_OR_NOT = {
+  'initial_snapshot': True,
+  'inserts_split': True,
+  'deletes': False,}
 
-  STATIC_NAMES = [
-    'Organisation',
-    'Organisation_isLocatedIn_Place',
-    'Place',
-    'Place_isPartOf_Place',
-    'Tag',
-    'TagClass',
-    'TagClass_isSubclassOf_TagClass',
-    'Tag_hasType_TagClass',
-  ]
-  DYNAMIC_NAMES = [
-    'Comment',
-    'Forum',
-    'Person',
-    'Post',
-    'Comment_hasCreator_Person',
-    'Comment_hasTag_Tag',
-    'Comment_isLocatedIn_Country',
-    'Comment_replyOf_Comment',
-    'Comment_replyOf_Post',
-    'Forum_containerOf_Post',
-    'Forum_hasMember_Person',
-    'Forum_hasModerator_Person',
-    'Forum_hasTag_Tag',
-    'Person_hasInterest_Tag',
-    'Person_isLocatedIn_City',
-    'Person_knows_Person',
-    'Person_likes_Comment',
-    'Person_likes_Post',
-    'Person_studyAt_University',
-    'Person_workAt_Company',
-    'Post_hasCreator_Person',
-    'Post_hasTag_Tag',
-    'Post_isLocatedIn_Country',
-  ]
-  NAMES = {'static':STATIC_NAMES, 'dynamic':DYNAMIC_NAMES}
+STATIC_NAMES = [
+  'Organisation',
+  'Organisation_isLocatedIn_Place',
+  'Place',
+  'Place_isPartOf_Place',
+  'Tag',
+  'TagClass',
+  'TagClass_isSubclassOf_TagClass',
+  'Tag_hasType_TagClass',
+]
+DYNAMIC_NAMES = [
+  'Comment',
+  'Forum',
+  'Person',
+  'Post',
+  'Comment_hasCreator_Person',
+  'Comment_hasTag_Tag',
+  'Comment_isLocatedIn_Country',
+  'Comment_replyOf_Comment',
+  'Comment_replyOf_Post',
+  'Forum_containerOf_Post',
+  'Forum_hasMember_Person',
+  'Forum_hasModerator_Person',
+  'Forum_hasTag_Tag',
+  'Person_hasInterest_Tag',
+  'Person_isLocatedIn_City',
+  'Person_knows_Person',
+  'Person_likes_Comment',
+  'Person_likes_Post',
+  'Person_studyAt_University',
+  'Person_workAt_Company',
+  'Post_hasCreator_Person',
+  'Post_hasTag_Tag',
+  'Post_isLocatedIn_Country',
+]
+NAMES = {'static':STATIC_NAMES, 'dynamic':DYNAMIC_NAMES}
 
-  client = storage.Client()
-  jobs = []
-  d1 ='initial_snapshot'
-  for d2 in ['static', 'dynamic']:
-    for name in NAMES[d2]:
-      loc = '/'.join([d1,d2,name]) + '/'
-      prefix = root + loc
-      target_dir = target / loc
+client = storage.Client()  
+jobs = []
+d1 ='initial_snapshot'
+for d2 in ['static', 'dynamic']:
+  for name in NAMES[d2]:
+    loc = '/'.join([d1,d2,name]) + '/'
+    prefix = root + loc
+    target_dir = target / loc
+    target_dir.mkdir(parents=True, exist_ok=True)
+    i = -1
+    for blob in client.list_blobs(bucket, prefix=prefix):
+      blob_name = blob.name
+      if not blob_name.endswith('.csv.gz'): continue
+      i += 1
+      if PARTITION_OR_NOT[d1] and i % args.nodes != args.index: continue
+      if name=='Comment' and i<100: print(name, i)
+      csv = blob_name.rsplit('/',1)[-1]
+      jobs.append((blob_name, target_dir/csv))
+
+for d1 in ['inserts_split','deletes']:
+  d2 = 'dynamic'
+  for name in NAMES[d2]:
+    loc = '/'.join([d1,d2,name]) + '/'
+    prefix = root + loc
+    i = -1
+    for blob in client.list_blobs(bucket, prefix=prefix):
+      blob_name = blob.name
+      if not blob_name.endswith('.csv.gz'): continue
+      i += 1
+      if PARTITION_OR_NOT[d1] and i % args.nodes != args.index: continue
+      batch, csv = blob_name.rsplit('/',2)[-2:]
+      if name=='Comment' and batch == 'batch_id=2012-11-29' and i<100: print(name, batch, i)
+      target_dir = target / loc / batch
       target_dir.mkdir(parents=True, exist_ok=True)
-      i = -1
-      for blob in client.list_blobs(bucket, prefix=prefix):
-        blob_name = blob.name
-        if not blob_name.endswith('.csv.gz'): continue
-        i += 1
-        if PARTITION_OR_NOT[d1] and i % args.nodes != args.index: continue
-        csv = blob_name.rsplit('/',1)[-1]
-        if name=='Comment' and i<100: print(name, i)
-        jobs.append((blob_name, target_dir/csv))
+      jobs.append((blob_name, target_dir/csv))
 
-  for d1 in ['inserts_split','deletes']:
-    d2 = 'dynamic'
-    for name in NAMES[d2]:
-      loc = '/'.join([d1,d2,name]) + '/'
-      prefix = root + loc
-      i = -1
-      for blob in client.list_blobs(bucket, prefix=prefix):
-        blob_name = blob.name
-        if not blob_name.endswith('.csv.gz'): continue
-        i += 1
-        if PARTITION_OR_NOT[d1] and i % args.nodes != args.index: continue
-        batch, csv = blob_name.rsplit('/',2)[-2:]
-        if name=='Comment' and i<100: print(name, batch, i)
-        target_dir = target / loc / batch
-        target_dir.mkdir(parents=True, exist_ok=True)
-        jobs.append((blob_name, target_dir/csv))
+def download(job):
+  client = storage.Client()  
+  gcs_bucket = client.bucket(bucket)
+  blob_name, target = job
+  gcs_bucket.blob(blob_name).download_to_filename(target)
 
-  #print('download to ', str(target))
-  print(f'start downloading {len(jobs)} files ...')
+#print('download to ', str(target))
+print(f'start downloading {len(jobs)} files ...')
+with Pool(processes=cpu_count()) as pool:
   pool.map(download,jobs)
-  print("downloading is done")
-
-if __name__ == '__main__':
-  download_one_partition(args.data)
+print("downloading is done")
