d11f8a67a64c3a7dc154abaf5679c3beab2365aa
Yuchen Zhang <yuchen.zhang@tigergraph.com>
2021-07-29 08:00:27 -0700

    download all

---
+++ ldbc_benchmark/tigergraph/queries_v3/LDBC_10TB/download_all.py
@@ -0,0 +1,53 @@
+import argparse
+import time
+import subprocess
+import paramiko
+from scp import SCPClient
+
+
+parser = argparse.ArgumentParser(description='Download and uncompress the data on all the nodes.')
+parser.add_argument('ip', type=str, help='starting ip address')
+parser.add_argument('nodes', type=int, help='the number of nodes')
+parser.add_argument('--data', '-d', type=str, default='10t' ,help='the data size. 10t or 30t')
+args = parser.parse_args()
+
+user = "tigergraph"
+pin = "tigergraph"
+workdir = '/home/tigergraph'
+def createSSHClient(server, port, user, password):
+    client = paramiko.SSHClient()
+    client.load_system_host_keys()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(server, port, user, password)
+    return client
+
+def main():
+  start_ip = args.ip.split('.')
+  targets = {
+  '10t':'sf10k',
+  '30t':'sf30k'}
+  for i in range(args.nodes):
+    ip4 = int(start_ip[-1]) + i
+    ip = start_ip[:-1] + [str(ip4)]
+    ip = '.'.join(ip)
+    ssh = createSSHClient(ip, 22, user, pin)
+    scp = SCPClient(ssh.get_transport())
+    target = '/home/tigergraph/' + targets[args.data]
+    
+    scp.put('download_one_partition.py', workdir)
+    scp.put('download_decompress.sh', workdir)
+    ssh.exec_command(f""" 
+      cd {workdir}
+      export index={i}
+      export nodes={args.nodes}
+      export data={args.data}
+      export target={target}
+      nohup sh download_decompress.sh > foo.out 2>&1 < /dev/null &  
+    """)
+    time.sleep(4)
+    ssh.exec_command('tail foo.out')
+    ssh.close()
+    scp.close()  
+
+if __name__ == '__main__':
+  main()
