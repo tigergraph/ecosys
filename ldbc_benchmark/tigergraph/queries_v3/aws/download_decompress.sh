7f97e229486115336ecbb72005f6368fd120fb2f
Yuchen Zhang <yuchen.zhang@tigergraph.com>
2021-09-04 16:10:48 -0700

    fix

--- ldbc_benchmark/tigergraph/queries_v3/aws/download_decompress.sh
+++ ldbc_benchmark/tigergraph/queries_v3/aws/download_decompress.sh
@@ -2,13 +2,16 @@
 export target=sf30k
 chmod 600 ~/.aws/credentials 
 # remove existing data
-echo "remove folder $target"
-rm -r $target
-echo 'done remove'
+if [ -d $target ]
+then
+  echo "remove folder $target"
+  rm -r $target
+  echo 'done remove'
+fi
 
 # download data
 echo "download date ($index/$nodes)"
-python3 -u download_one_partition.py $index $nodes && \
+python3 -u download_one_part.py $index $nodes && \
 echo 'done download' && \
 echo "deompose files in $target" && \
 find $target -name *.csv.gz  -print0 | parallel -q0 gunzip && \
