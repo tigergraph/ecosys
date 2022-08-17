e2fc03f67bf4dfe8b354d3b7a1cb85d048d0da94
Yiming Pan <56082650+YimingPan-Code@users.noreply.github.com>
2022-08-17 15:47:24 -0700

    Update minHash.cpp

--- scripts/TokenBank/minHash.cpp
+++ scripts/TokenBank/minHash.cpp
@@ -1,10 +1,161 @@
-#include <iostream>
-#include <vector>
-#include <string.h>
-#include <string>
-#include <assert.h>
-#include <algorithm>
+/******************************************************************************
+ * Copyright (c) 2014, 2016, TigerGraph Inc.
+ * All rights reserved.
+ * Project: TigerGraph Loader
+ * TokenBank.cpp: a library of token conversion function declaration.
+ *
+ * - It is an N-tokens-in, one-token-out function. N is one or more.
+ * - All functions must use one of the following signatures,
+ *   but different function name.
+ * - A token function can have nested other token function;
+ *   The out-most token function should return the same type
+ *   as the targeted attribute type specified in the
+ *   vertex/edge schema.
+ *
+ *   1. string[] -> string
+ *
+ *   The UDF token conversion functions will take N input char
+ *   array and do a customized conversion. Then, put the
+ *   converted char array to the output char buffer.
+ *
+ *     extern "C" void funcName (const char* const iToken[], uint32_t iTokenLen[],
+ *     uint32_t iTokenNum, char* const oToken, uint32_t& oTokenLen);
+ *
+ *      @param: iToken: 1 or more input tokens, each pointed by one char pointer
+ *      @param: iTokenLen: each input token's length
+ *      @param: iTokenNum: how many input tokens
+ *      @param: oToken: the output token buffer; caller will prepare this buffer.
+ *      @param: oTokenLen: the output token length
+ *
+ *      Note: extern "C" make C++ compiler not change/mangle the function name.
+ *      Note: To avoid array out of boundary issue in oToken buffer, it is
+ *            recommended to add semantic check to ensure oToken length does
+ *            not exceed  OutputTokenBufferSize parameter specified in the
+ *            shell config. Default is 2000 chars.
+ *
+ *
+ *
+ *   2. string[] -> int/bool/float
+ *
+ *     extern "C" uint64_t funcName (const char* const iToken[],
+ *     uint32_t iTokenLen[], uint32_t iTokenNum)
+ *
+ *     extern "C" bool funcName (const char* const iToken[], uint32_t iTokenLen[],
+ *     uint32_t iTokenNum)
+ *
+ *     extern "C" float funcName (const char* const iToken[], uint32_t iTokenLen[],
+ *     uint32_t iTokenNum)
+ *
+ *      @param: iToken: 1 or more input tokens, each pointed by one char pointer
+ *      @param: iTokenLen: each input token's length
+ *      @param: iTokenNum: how many input tokens
+ *
+ *   Think token function as a UDF designed to combine N specific columns into
+ *   one column before we load them into graph store.
+ *
+ * - All functions can be used in the loading job definition, in the VALUES caluse.
+ *    e.g. Let a function named Concat(), we can use it in the DDL shell as below
+ *      values( $1, Concat($2,$3), $3...)
+ *
+ *
+ * - Once defined UDF, run the follow to compile a shared libary.
+ *
+ *    TokenBank/compile
+ *
+ *   TigerGraph loader binary will automatically use the library at runtime.
+ *
+ * - You can unit test your token function in the main function in this file.
+ *   To run your test, you can do
+ *
+ *     g++ TokenBank.cpp
+ *     ./a.out
+ *
+ * Created on: Dec 11, 2014
+ * Updated on: July 19, 2016
+ * Author: Mingxi Wu
+ ******************************************************************************/
 
+#include <stdio.h>
+#include <stdint.h>
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <string>
+#include <stdbool.h>
+#include <cstdlib>
+#include <assert.h>
+#include <string.h>
+#include "TokenLib.hpp"
+
+ /**
+  * this function concatenate all input tokens into one big token
+  *
+  */
+/*extern "C" void _Concat(const char* const iToken[], uint32_t iTokenLen[], uint32_t iTokenNum,
+    char* const oToken, uint32_t& oTokenLen){
+
+  uint32_t k = 0;
+  for (uint32_t i=0; i < iTokenNum; i++){
+    for (uint32_t j =0; j < iTokenLen[i]; j++) {
+           oToken[k++]=iToken[i][j];
+    }
+  }
+  oTokenLen = k;
+}*/
+
+/**
+ * This function convert iToken char array of size iTokenLen, reverse order
+ * and put it in oToken.
+ *
+ * Only one token input.
+ */
+extern "C"  void Reverse (const char* const iToken[], uint32_t iTokenLen[], uint32_t iTokenNum,
+    char *const oToken, uint32_t& oTokenLen) {
+
+  uint32_t j = 0;
+  for (uint32_t i = iTokenLen[0]; i > 0; i--) {
+    oToken[j++] = iToken[0][i-1];
+  }
+  oTokenLen = j;
+}
+
+/**
+ * This function convert iToken to integer and compare with 3.
+ * If it is greater than 3, return true. Otherwise, return false.
+ *
+ * Only one token input.
+ */
+extern "C"  bool GreaterThan3(const char* const iToken[], uint32_t iTokenLen[], uint32_t iTokenNum) {
+
+  int tmp = atoi(iToken[0]);
+
+  if (tmp >3) {
+    return true;
+  }
+  return false;
+}
+
+/**
+ * This function convert each input token to an integer, sum them and compare
+ * the sum with 3. If it is greater than 3, return true. Otherwise, return false.
+ *
+ */
+extern "C" bool SumGreaterThan3(const char* const iToken[], uint32_t iTokenLen[], uint32_t iTokenNum)
+{
+  int k = 0;
+  int sum = 0;
+
+  for (uint32_t i=0; i < iTokenNum; i++) {
+    int tmp = atoi(iToken[i]);
+    sum += tmp;
+  }
+
+  if (sum > 3) {
+    return true;
+  }
+
+  return false;
+}
 uint32_t MurmurHash2 ( const void * key, int len, uint32_t seed )
 {
   // 'm' and 'r' are mixing constants generated offline.
@@ -56,24 +207,21 @@ uint32_t MurmurHash2 ( const void * key, int len, uint32_t seed )
   return h;
 } 
 
-void minHash (const char* const iToken[], uint32_t iTokenLen[], uint32_t iTokenNum,
+extern "C" void minHash (const char* const iToken[], uint32_t iTokenLen[], uint32_t iTokenNum,
     char* const oToken, uint32_t& oTokenLen) {
   //minHash take three input (int str, int shingleLen, int b, int r)
-  assert(iTokenNum == 4);
   uint32_t shingleLen = atoi(iToken[1]);
   int b = atoi(iToken[2]);
-  int r = atoi(iToken[3]);
   //only handle the case for the number of hash functions is less or equal to 100
-  assert(b*r <= 100);
   uint32_t k = std::min(iTokenLen[0], shingleLen);
   char tmp[k];
   std::vector<uint32_t> signatures;
-  std::cout << "start signatures\n";
+  //std::cout << "start signatures\n";
   for (int i = 0; i < iTokenLen[0] - k + 1; ++i) {
     signatures.push_back(MurmurHash2(&iToken[0][i], k, 0));
-    std::cout << "token: " << std::string(&iToken[0][i], k) << ", MurmurHash2: " <<  MurmurHash2(&iToken[0][i], k, 0) << std::endl;
+    //std::cout << "token: " << std::string(&iToken[0][i], k) << ", MurmurHash2: " <<  MurmurHash2(&iToken[0][i], k, 0) << std::endl;
   }
-  std::cout << "end signatures\n";
+  //std::cout << "end signatures\n";
   //x = (a*x + b) % p
   uint64_t p = 4294967311; //the prime number bigger than 2^32 - 1
   uint32_t c1[101] = {1, 482067061, 1133999723, 2977257653, 2666089543, 3098200677, 3985189319, 1857983931, 3801236429, 522456919, 4057595205, 4176190031, 1652234975, 2294716503, 1644020081, 3407377875, 3749518465, 4244672803, 3053397733, 3273255487, 598272097, 3989043777, 1414109747, 697129027, 67285677, 98002333, 158583451, 1424122447, 2159224677, 3478101309, 277468927, 1902928727, 2459288935, 3941065327, 1244061689, 1898521317, 4205778491, 1987240989, 3446018461, 2407533397, 3151958893, 1553147067, 208156801, 2362352445, 2458343227, 4134443, 36216853, 932983869, 2800766507, 252990279, 2994662963, 2760285623, 4510445, 1458512423, 3500568231, 689831701, 887836659, 315834449, 2394075311, 1360826347, 439713319, 633358329, 749540625, 444867375, 531150885, 2871421439, 2347294453, 3975247983, 3255073387, 3561466319, 2616895667, 742825395, 3300710079, 1231551531, 3576325163, 3229203393, 2662941725, 3495109109, 2202779339, 2997513035, 1952088617, 2177967115, 1685362661, 2160536397, 2628206479, 1678152567, 775989269, 2114809421, 3882162141, 3267509575, 3869378355, 283353181, 306744579, 2793152333, 1454134621, 3021652657, 1664069155, 1711688171, 1264486497, 359065375, 1616608617};
@@ -86,17 +234,15 @@ void minHash (const char* const iToken[], uint32_t iTokenLen[], uint32_t iTokenN
     if (i > 0) {
       hashStr.push_back('|');
     }
-    //loop for block
-    for (int j = 0; j < r; ++j) {
-      uint32_t minHash = 0xFFFFFFFF;
-      for (auto s : signatures) {
-        uint32_t x = (c1[idx++] * s + c2[idx++]) % p;
-        if (x < minHash) {
-          minHash = x;
-        }
+    uint64_t minHash = 0xFFFFFFFF;
+    for (auto s : signatures) {
+      uint64_t x = (c1[idx] * s + c2[idx]) % p;
+      if (x < minHash) {
+        minHash = x;
       }
-      hashStr += std::to_string(minHash);
     }
+    hashStr += std::to_string(minHash);
+    idx++;
   }
   oTokenLen = hashStr.size();
   for (int i = 0; i < hashStr.size(); ++i) {
@@ -105,25 +251,30 @@ void minHash (const char* const iToken[], uint32_t iTokenLen[], uint32_t iTokenN
   oToken[oTokenLen] = '\0';
 }
 
-/* Command: ./a.out xinyu@gmail.com k b r
- * where k is the length for each shingle
- * b is the number of blocks
- * r is the number of hash functions for each block
- * b * r should be smaller than 100, because we only provide 100 token functions
- * */
-int main(int argc, char** argv){
-  char* email[4];
-  email[0]=argv[1];
-  email[1]=argv[2];
-  email[2]=argv[3];
-  email[3]=argv[4];
-  uint32_t iTokenLen[4];
-  iTokenLen[0]=strlen(argv[1]);
-  iTokenLen[1]=strlen(argv[2]);
-  iTokenLen[2]=strlen(argv[3]);
-  iTokenLen[3]=strlen(argv[4]);
-  char mh[500];
-  uint32_t outlen = 0;
-  minHash(email, iTokenLen, 4, mh, outlen);
-  std::cout << "email: " << argv[1] << ", hashCode: "<< mh << std::endl;
+/**
+ *  Unit testing of the token bank functions
+ */
+/*int main(){
+
+  char* a[2];
+  char d[100]={'a','b','c','d','e','f'};
+
+  a[0]=&d[0];
+  a[1]=&d[3];
+
+  uint32_t len[2];
+
+  len[0] = 3;
+  len[1] = 3;
+
+
+  char b[100];
+  uint32_t  outlen;
+  _Concat(a,len,2, b, outlen);
+  for(int i =0; i<outlen; i++){
+    std::cout<<b[i]<<",";
+  }
+  std::cout<<std::endl;
+
 }
+*/
